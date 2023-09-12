9ef21f77e8cb619556cc4c874f3009bc673cba82
Andy Liang <100326980+GrumpyMusician@users.noreply.github.com>
2023-09-12 08:30:35 -0500

    Progress on sololab name

--- Labs/sololabs_name.java
+++ Labs/sololabs_name.java
@@ -1,9 +1,9 @@
 
 /**
- * Write a description of class sololabs_name here.
- *
- * @author (your name)
- * @version (a version number or a date)
+ * Andy Liang
+ * 09/12/23
+ * Name
+ * Returns name inputted by user
  */
 
 import java.util.Scanner;
@@ -12,11 +12,38 @@ public class sololabs_name
 {
     public static void main(String[] args)
     {
-        String name = new String();
+        String sectionThree = new String();
+        String sectionOne = new String();
+        String sectionTwo = new String();
+        
         Scanner input = new Scanner(System.in);
         
-        System.out.println("Input in your full name (first, middle, last, seperated by white space:");
-        name = input.nextLine(); 
+        System.out.println("Input in your full name (first, middle, last, seperated by white space:)");
+        sectionThree = input.nextLine(); 
+        
+        int spaceIndex = sectionThree.indexOf(" ");
+        sectionOne = sectionThree.substring(0, spaceIndex);
+        sectionThree = sectionThree.substring(spaceIndex+1);
+        
+        spaceIndex = sectionThree.indexOf(" ");
+        sectionTwo = sectionThree.substring(0, spaceIndex);
+        sectionTwo = sectionTwo.substring(0, 1);
+        sectionThree = sectionThree.substring(spaceIndex+1);
+        
+        System.out.println("Your name is: " + sectionOne +" "+ sectionTwo +". "+ sectionThree);
         
     }
 }
+
+/**
+ * Output One:
+ * Input in your full name (first, middle, last, seperated by white space:)
+ * Olympic Titanic Britannic
+ * Your name is: Olympic T. Britannic
+ * 
+ * Output Two:
+ * Input in your full name (first, middle, last, seperated by white space:)
+ * The Old Reliable
+ * Your name is: The O. Reliable
+ */
+
